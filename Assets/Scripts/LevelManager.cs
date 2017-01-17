eeebd4536f578617d75ba8df9e366294a2eaced4
John McCabe <john@johnmccabe.net>
2017-01-17 00:18:31 +0000

    importing the menu system

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level load requested for: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest () {
+		Debug.Log ("QuitRequest called");
+		Application.Quit ();
+	}
+
+}
