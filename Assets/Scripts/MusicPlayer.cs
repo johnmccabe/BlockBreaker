a516028e5d16dd485beb292697af6d6ca899e102
John McCabe <john@johnmccabe.net>
2017-01-18 00:09:32 +0000

    use static var to create singleton
    prevents MusicPlayer instance get created each time you return to the
    Start scene

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
 
+	static MusicPlayer instance = null;
+
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad (gameObject);
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
 	}
 }
