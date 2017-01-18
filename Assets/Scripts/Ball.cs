026905d53b116039d6e8615612943f1c0e08f7ec
John McCabe <john@johnmccabe.net>
2017-01-18 23:47:19 +0000

    launching ball on mouse click
    note the script execution ordering to make sure we have a paddle
    position before attaching the ball to that position

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		print (paddleToBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		}
+
+		// Wait for a mouse press to launch
+		if ( Input.GetMouseButtonDown(0) ) {
+			print("Mouse Clicked, launch ball");
+
+			hasStarted = true;
+			gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+		}
+	}
+}
