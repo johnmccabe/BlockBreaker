9e6af2160b8ad7dcc6ac1debe626c0b2547bd8a1
John McCabe <john@johnmccabe.net>
2017-01-22 00:33:23 +0000

    tidy paddle chamfer and expose min/max setting

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
 
 	public bool autoPlay = false;
+	public float minX,  maxX;
+
 
 	private Ball ball;
 
@@ -24,7 +26,7 @@ public class Paddle : MonoBehaviour {
 	void MoveWithMouse () {
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-		paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
 		this.transform.position = paddlePos;	
 	}
 
